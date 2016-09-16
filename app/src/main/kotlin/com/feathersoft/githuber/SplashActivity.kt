ad00b967e86201873d736818154570a0fce15713
Jorge Mendez <jmendez.garmendia@gmail.com>
2016-09-15 19:58:03 -0700

    Refactored project name

--- app/src/main/kotlin/com/feathersoft/githuber/SplashActivity.kt
+++ app/src/main/kotlin/com/feathersoft/githuber/SplashActivity.kt
@@ -3,11 +3,10 @@ package com.feathersoft.githubber
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_home)
+    setContentView(R.layout.activity_splash)
   }
-
 }
