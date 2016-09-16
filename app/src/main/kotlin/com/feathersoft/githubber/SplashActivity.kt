5c3f127cb05263fd860d7aefc881bdf5916baca6
Jorge Mendez <jmendez.garmendia@gmail.com>
2016-09-15 21:02:43 -0700

    Adds loading animation to splash screen

--- app/src/main/kotlin/com/feathersoft/githubber/SplashActivity.kt
+++ app/src/main/kotlin/com/feathersoft/githubber/SplashActivity.kt
@@ -1,12 +1,25 @@
 package com.feathersoft.githubber
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import butterknife.BindView
+import com.feathersoft.githubber.architecture.GitHubberActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : GitHubberActivity() {
+
+  @BindView(R.id.splash_img_github_shadow) lateinit var mImageGithubShadow: ImageView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
   }
+
+  override fun setupContent() {
+    mImageGithubShadow.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation))
+  }
+
+  override fun getContentViewRes(): Int {
+    return R.layout.activity_splash
+  }
+
 }
---
+++ app/src/main/kotlin/com/feathersoft/githubber/architecture/GitHubberActivity.kt
@@ -0,0 +1,46 @@
+package com.feathersoft.githubber.architecture
+
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+
+/**
+ * Created by jormendez on 9/15/16.
+ */
+abstract class GitHubberActivity: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setupContentView()
+  }
+
+  open fun setupContentView() {
+    setContentView(getContentViewRes())
+    ButterKnife.bind(this)
+    setupContent()
+  }
+
+  fun rootView(): View {
+    var rootView = findViewById(android.R.id.content)
+    if(rootView is ViewGroup) {
+      return rootView.getChildAt(0)
+    } else {
+      return rootView
+    }
+  }
+
+  /**
+   * Will be called when the GitHubberActivity attempts to set the content view
+   */
+  abstract @LayoutRes fun getContentViewRes(): Int
+
+  /**
+   * Will be called once the content view layout has been set, and the views has been bound. Every
+   * view content and listener must be set during this callback
+   */
+  abstract fun setupContent()
+
+}
